ba10ca8295ade52b17865e33c55fc4d3fd601b9c
Yonka 2019 <yonka2003@gmail.com>
2022-01-14 21:23:26 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+
+internal class Program
+{
+    private static readonly string host = ConfigurationManager.AppSettings["Host"];
+    private static void Main()
+    {
+        Ping ping = new Ping();
+        PingReply reply;
+        if (host == null)
+        {
+            Console.WriteLine("[ERROR] Can't find configuration file");
+
+            Console.WriteLine("Press any button...");
+            Console.ReadKey();
+
+            Environment.Exit(0);
+        }
+        Console.WriteLine($"[HOST] |{host}|: Pinging...");
+
+        while (true)
+        {
+            try
+            {
+                reply = ping.Send(host);
+            }
+            catch
+            {
+                Shutdown();
+                return;
+            }
+            if (reply.Status != IPStatus.Success)
+            {
+                Shutdown();
+            }
+        }
+    }
+    private static void Shutdown()
+    {
+        Console.WriteLine($"[ERROR] |{host}|: Shutting down...");
+        Process.Start("shutdown", "/s /t 0");  // Shutdowns PC
+    }
+}
